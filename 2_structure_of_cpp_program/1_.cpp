04b95204ae8d4a3cb2a860d2762e7e2672e8b24b
dylanlooyq <dylanlyq@gmail.com>
2025-05-10 23:16:19 +0800

    cleanup

---
+++ 2_structure_of_cpp_program/1_.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// using namespace std;
+
+// // There are 90 keywords in C++
+
+
+// int main() {
+
+//     int fav_num;
+//     cout << "Enter your favourite number" << endl;
+//     cin >> fav_num;
+//     cout << "Your fav number is:" << fav_num << endl;
+
+//     return 0;
+// }
+
+#include <iostream>
+#include "1_myfile.h"
+using namespace std;
+
+main () {
+    int data1;
+    double data2;
+
+    cout << "Give me 2 numbers" << endl;
+    cin >> data1 >> data2;
+    cout << "First number is: " << data1 << endl;
+    cout << "Second number is: " << data2 << endl;
+
+    double data3 = data1 + data2;
+    cout << "The sum is " << data3 << endl;
+
+    cout << "Press enter to continue..." << endl;
+    cin.ignore();
+    cin.get();
+}
---
+++ 3_variables_and_constants/3_.cpp
@@ -0,0 +1,5 @@
+/* C++ primitive types
+- Integer
+- Float
+- Boolean
+- Character */
