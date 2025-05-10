5975e23ffe5570f40ae122d382d158752772b4f8
dylanlooyq <dylanlyq@gmail.com>
2025-05-10 21:27:58 +0800

    Completed section challenge for chapter 1

--- 1_getting_started/6_section_challenge.cpp
+++ 1_getting_started/6_section_challenge.cpp
@@ -5,23 +5,62 @@
 // "Amazing!! That's my favourite number too!!"
 // "No really!! 24 is my favourite number!!"
 
+
+//// VERSION 1 WITH NO TIME ELAPSED ////
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int favourite_number;
+//     cout << "Enter your favourite number" << endl;
+//     cin >> favourite_number;
+
+//     if (favourite_number >= 1 && favourite_number <= 100) {
+//         cout << "Amazing!! That's my favourite number too!!" << endl;
+//         cout << "No really!! " << favourite_number << " is my favourite number!!" << endl;
+//     }
+
+//     else {
+//         cout << "Please enter a number between 1 and 100" << endl;
+//     }
+
+//     cout << "press enter to continue..." << endl;
+//     cin.ignore();
+//     cin.get();
+// }
+
+//// VERSION 2 WITH TIME ELAPSED ////
 #include <iostream>
+#include <chrono>
+#include <cmath>
+
 using namespace std;
+using namespace chrono;
 
 int main() {
-    int favourite_number;
-    cout << "Enter your favourite number" << endl;
-    cin >> favourite_number;
+    auto start = chrono::high_resolution_clock::now();
 
-    if (favourite_number < 1 || favourite_number > 100) {
-        cout << "Amazing!! That's my favourite number too!!" << endl;
-        cout << "No really!! " << favourite_number << " is my favourite number!!" << endl;
-        return 0; // Return a non-zero value to indicate an error
-    }
+    while (true) {
+        int favourite_number;
+        cout << "Enter your favourite number" << endl;
+        cin >> favourite_number;
 
-    if (favourite_number) {
+        if (favourite_number >= 1 && favourite_number <= 100) {
+            cout << "Amazing!! That's my favourite number too!!" << endl;
+            cout << "No really!! " << favourite_number << " is my favourite number!!" << endl;
+            break;
+            }
 
-    }
+        else {
+            cout << "Please enter a number between 1 and 100" << endl;
+            break;
+            }
+    };
+
+    auto end = high_resolution_clock::now();
+    duration <double> elapsed = end - start;
+    double elapsed_rounded = round(elapsed.count()*100)/100;
+    cout << "Time elapsed: " << elapsed_rounded << " seconds" << endl;
 
     cout << "press enter to continue..." << endl;
     cin.ignore();
